19f64989f4b20c0cdaac5ec32e5347d26967b77b
Philcob Suzuki Josol <76026580+pjguitar15@users.noreply.github.com>
2021-11-17 08:17:52 +0800

    Update CollegeList.java

--- Java Exercises/People in School/CollegeList.java
+++ Java Exercises/People in School/CollegeList.java
@@ -1,52 +1,57 @@
+package peopleinschool;
+
 import java.util.Scanner;
 
+/**
+ *
+ * @author Philcob
+ */
 public class CollegeList {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        
+        // take user choice input as character
         System.out.print("Press E for Employee, F for Faculty, or S for Student: ");
         char userChoice = sc.next().toLowerCase().charAt(0);
-
+        
+        // moves cursor to next Scanner input. without this there will be issues with Scanner
+        sc.nextLine();
+        
         if (userChoice == 'e') {
             System.out.println("Type employee's name, contact number, salary, and department. Press Enter after every input.");
             
-            // this is required to move to the next cursor after the char scanner input
-            sc.nextLine();
-            String name = sc.nextLine();                                         
+            String name = sc.nextLine();
             String contactNum = sc.nextLine();
             double salary = sc.nextDouble();
 
-            // need this to prevent Scanner skipping input!!!
-            sc.nextLine();            
-
-            String department = sc.nextLine();
+            // prevents Scanner skipping input
+            sc.nextLine();
             
-            // Employee object
+            // take department input (last input)
+            String department = sc.nextLine();
+
+            // create an instance of Employee object
             Employee employee = new Employee();
             employee.setName(name);
             employee.setContactNum(contactNum);
             employee.setSalary(salary);
             employee.setDepartment(department);
 
-            // display employee entered
+            // display employee information
             System.out.println("-------------------------------");
             System.out.println("Name: " + employee.getName());
             System.out.println("Contact Number: " + employee.getContactNum());
             System.out.println("Salary: " + employee.getSalary());
             System.out.println("Department: " + employee.getDepartment());
+            
         } else if (userChoice == 's') {
             System.out.println("Type student's name, contact number, program, and year level. Press Enter after every input.");
             
-            // this is required to move to the next cursor after the char scanner input
-            sc.nextLine();
-            String name = sc.nextLine();                                         
+            String name = sc.nextLine();
             String contactNum = sc.nextLine();
-            String program = sc.nextLine();        
+            String program = sc.nextLine();
             int yearLevel = sc.nextInt();
-            
-            System.out.println("Name: " + name);
-            System.out.println("Contact number: " + contactNum);
-            System.out.println("Program: " + program);
-            System.out.println("Year level: " + yearLevel);
 
             // set student object
             Student student = new Student();
@@ -61,15 +66,52 @@ public class CollegeList {
             System.out.println("Contact Number: " + student.getContactNum());
             System.out.println("Program: " + student.getProgram());
             System.out.println("Year level: " + student.getYearLevel());
-            
-            
-        } else if (userChoice == 'f') {
-            
+
+        } else if (userChoice == 'f') {            
+            System.out.print("Are you regular/tenured faculty member? Y/N: ");
+            char response = sc.next().toLowerCase().charAt(0);
+
+            System.out.println("Type employee's name, contact number, salary, and department. Press Enter after every input.");
+
+            // required itong sc.nextLine() para mag move yung Scanner cursor sa String name
+            sc.nextLine();
+            String name = sc.nextLine();
+            String contactNum = sc.nextLine();
+            double salary = sc.nextDouble();
+
+            // need this to prevent Scanner skipping input!!!
+            sc.nextLine();
+
+            String department = sc.nextLine();
+            // Employee object
+            Faculty faculty = new Faculty();
+            faculty.setName(name);
+            faculty.setContactNum(contactNum);
+            faculty.setSalary(salary);
+            faculty.setDepartment(department);
+
+            if (response == 'y') {
+                faculty.isRegular(true);
+            } else {
+                faculty.isRegular(false);
+            }
+
+            // display employee entered
+            System.out.println("-------------------------------");
+            System.out.println("Name: " + faculty.getName());
+            System.out.println("Contact Number: " + faculty.getContactNum());
+            System.out.println("Salary: " + faculty.getSalary());
+            System.out.println("Department: " + faculty.getDepartment());
+
+            // Gumamit ako ng ternary operator na ? and :   medyo advanced topic nato pero pwede mo din i-google
+            System.out.println("Status: " + (faculty.isStatus() ? "Regular" : "Irregular"));
         }
     }
+
 }
 
 class Person {
+
     private String name;
     private String contactNum;
 
@@ -87,10 +129,11 @@ class Person {
 
     public String getContactNum() {
         return contactNum;
-    }    
-} // end of Person class
+    }
+}
 
-class Student extends Person{
+class Student extends Person {
+
     private String program;
     private int yearLevel;
 
@@ -108,10 +151,11 @@ class Student extends Person{
 
     public int getYearLevel() {
         return yearLevel;
-    }    
-} // end of Student class
+    }
+}
 
-class Employee extends Person{
+class Employee extends Person {
+
     private double salary;
     private String department;
 
@@ -132,12 +176,16 @@ class Employee extends Person{
     }
 }
 
-class Faculty {
+class Faculty extends Employee {
+
     private boolean status;
 
-    public boolean isRegular(boolean status){
-        return true;
+    public void isRegular(boolean status) {
+        this.status = status;
     }
+
+    public boolean isStatus() {
+        return status;
+    }
+
 }
-
-
